6cb6d2e0e42518a4d4f023e5950553074943b3ce
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-13 01:15:05 +0530

    Add Sample.py

---
+++ Sample.py
@@ -0,0 +1,150 @@
+print("Hello World")
+print(5) 
+print("Hello\n W \n W")  
+print("Tejas,og meme,Hello world")
+
+# Variables in python 
+
+T = 2
+E = "Tejas"
+print(E)
+
+
+
+
+
+OK = {"Name":"Tejas Khaire", "Age":20}
+print(OK)
+print(5+5)
+print(5*5)
+print(2**4)
+print(15/6)
+print(3%2)
+print(15//2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+NUM1 = "2"
+Num2 = "3"
+print(int(NUM1)+int(Num2))
+
+
+a = 1.5
+b = 2
+print(a+b)
+
+
+
+
+
+
+"""print("Enter your name: ")
+TE = input()
+print("My Name is "+TE)"""
+
+"""print("Enter Your NUM1: ")
+X = input()
+print("Enter Your NUM2: ")
+Y = input()
+print(int(Y) + int(X))
+"""
+
+
+"""TE = "Hello"
+print(TE[1])
+
+TEJ = "Hello World"
+print(TEJ[0:5])
+print(len(TEJ))"""
+
+
+
+
+
+"""print("Hello")
+print("Enter Your Number1: ")
+T = input()
+print("Enter Your Number2: ")
+E = input()
+print(int(T)+int(E))
+"""
+"""print("What is your Name: ")
+T = input()
+print("My Name is "+T)"""
+
+
+"""Name  = "Tejas Khaire"
+print(len(Name))
+print(Name.upper())
+print(Name.lower())
+print(Name.rstrip("i"))
+print(Name.replace("Tejas","Bandu"))
+print(Name.split(" "))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
