c455df51dc7ddcf5cda58a87019955a312d170e2
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-13 01:21:28 +0530

    Add if else

---
+++ else.py
@@ -0,0 +1,22 @@
+"""print("enter your age: ")
+T = int(input())
+if (T>18):
+    print("Your are eligible to drive")
+else:
+    print("Sorry, your not eligible")"""
+ 
+"""print("Enter Time: ")
+T = float(int())
+if (T>6 & T<12):
+    print("Good Moring")
+elif(T<12 & T>)"""
+    
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
