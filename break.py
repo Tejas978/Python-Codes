11b4d5323222fddc73a244fec1c770ff5fe21a03
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-14 13:09:25 +0530

    break statement code

---
+++ break.py
@@ -0,0 +1,10 @@
+"""for i in range(15):
+    if (i==10):
+        break
+    print("5 X ",i+1,"=",5*(i+1))"""
+
+for i in range(15):
+ if (i==10):
+    print("Please Leave These Statement")
+    continue
+ print("5 X ",i,"=",5*(i))
