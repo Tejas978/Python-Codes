393010f236fac54cfe2a7be7cad17cece53c796c
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-14 13:08:26 +0530

    added loop file

---
+++ loop.py
@@ -0,0 +1,15 @@
+"""Name = 'Tejas'
+for i in Name:
+    print(i)
+    if (i=="j"):
+        print("J for Joker")"""
+
+"""for r in range(10):
+    print(r+1)"""
+
+"""for r in range(100,2000,100):
+    print(r)"""
+
+"""name = "TEJASKHIARE"
+for t in name:
+    print(t)"""
