db94c908cddfd925f152d42c31b291167264cce0
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-15 10:59:28 +0530

    List Understanding

---
+++ list.py
@@ -0,0 +1,58 @@
+'''List = [0,1,2,3,"Tejas","Khaire",True]
+print(List)'''
+'''print(List[0])
+print(List[1])
+print(List[2])
+print(List[3])
+print(List[4])
+print(List[5])
+print(List[6])'''
+ 
+"""if "Tejas" in List:
+    print("I am IN")
+else:
+    print("I am OUT")"""
+
+'''
+print(List[-3])
+print(List[len(List)-3])
+'''''''
+print(List[0:7])
+print(List[:])
+print(List[2:6])
+print(List[0:7:2])'''
+
+'''List = [i+1 for i in range(20) if (i)%2]
+print(List)
+
+
+
+
+
+
+
+
+
+
+Tej = [T for T in range(20) if T%1]
+print(Tej)'''
+
+
+
+'''
+ls = [i+1 for i in range(10)]
+print(ls)'''
+'''List.append(11)'''
+
+ls = [1,3,5,4,5]
+ls.append(5)
+ls.sort()
+print(ls)
+
+
+
+
+
+
+
+
