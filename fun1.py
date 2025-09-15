d95063effa520d71893d3b9f83ce1e078713e933
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-15 10:58:48 +0530

    Fun1 update

---
+++ fun1.py
@@ -0,0 +1,31 @@
+"""def vnum(a,b):
+    print("The Value is : ", (a+b)*2)
+
+vnum(5,6)   #Required Arguments"""
+ 
+"""def vnum(a=5,b=10):
+    print("The Value is : ", (a+b)*2)
+
+vnum(4)     #Default Arguments"""
+
+"""def vnum(a,b):
+    print("The Value is : ", (a+b)*2)
+
+vnum(5,6)
+vnum(b=6,a=5)""" #Keyword Arguments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
