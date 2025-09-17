07c80c02472893f47f4adcfecd61f4b5e0f0da04
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-17 19:57:42 +0530

    String Concept

---
+++ f_string.py
@@ -0,0 +1,7 @@
+'''T = "My Name is {1} and I am from {0}."
+name = "Tejas"
+city = "Mumbai"
+print(T.format(name,city))'''
+'''name = "Tejas"
+city = "Mumbai"'''
+print(f"My Name is {{name}} and I am from {{city}}.")
