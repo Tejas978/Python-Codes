898abb5a036264bee07a602b9d88a206a119a7f3
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-22 01:24:03 +0530

    Understanding Decorator/Greet@ in Python

---
+++ Dec.py
@@ -0,0 +1,137 @@
+'''def greet(fx):
+    def tej():
+        print("Good Morning")
+        fx()
+        print("Good Night")
+    return tej
+@greet
+def hello():
+    print("Good After-Noon")
+
+hello()'''
+print("Hello World!!!")
+
+'''def greet(fine):
+    def two(*args,**kwargs):
+      print("Enter you Number:a,b")
+      fine(*args,**kwargs)
+      print("Thanks for using ADD function")
+    return two
+@greet
+def add(a,b):
+   print(int(a)+int(b))
+add(6,4)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
