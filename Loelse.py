51fbc3fec62d4410ebf076c930bdc20a4612d265
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-09-25 23:26:20 +0530

    Understanding else

---
+++ Loelse.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i)
+    if i==3:
+        break
+else:
+    print("Hello World")
+i=0
+while i<5:
+    print(i)
+    i = i + 1
+    if i==2:
+     break
+else:
+    print("Hello")
