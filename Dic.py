e18329824702e5e539780f9df2970b3adf519f61
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-10-07 20:22:26 +0530

    Understanding Dictionary

---
+++ Dic.py
@@ -0,0 +1,17 @@
+dic = {"Name":"Tejas", "Age":20,"City":"Mumbai"} #Variablename can be anything(dic,T..)
+print(dic)
+''''print(dic["Name"])
+print(dic["Age"])
+print(dic["City"])'''
+
+''''print(dic.keys())
+print(dic.values())'''
+
+'''for i in dic.values():
+    print(i)
+    '''
+#print(dic.items())
+
+dic1 = {"Proper":"CHD","Live":"Hostel"}
+dic.update(dic1)
+print(dic)
