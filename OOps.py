51addd5494a5f81688e051cb24386a890bfeb28e
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-10-13 23:51:36 +0530

    Object Oriented

---
+++ OOps.py
@@ -0,0 +1,28 @@
+print("Hello World!!!")
+class student:
+    Name = "Tejas"
+    Age = 21
+    Roll = 81
+    Branch  = "IT"
+    Div = 'B'
+    def hello(self):
+        print(f"I am {self.Name} and I am {self.Age} with roll {self.Roll} ")
+obj1 = student()
+obj1.Name = "Harry"
+# print(obj1.Name)
+obj1.hello()
+'''print(obj1.Name)
+print(obj1.Age)
+print(obj1.Roll)
+print(obj1.Branch)
+print(obj1.Div)
+obj2 = student()
+obj2.Name = "Mahesh"
+obj2.Age = 21
+obj2.Roll = 83
+obj2.Branch = 'CS'
+obj2.Div = 'A'
+print(obj2.Name,obj2.Age,obj2.Roll,obj2.Branch,obj2.Div)
+'''
+
+
