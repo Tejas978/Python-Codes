198561a14a6d26b63068cda42037754488221548
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-10-14 19:44:54 +0530

    OS practise

---
+++ OSpractise.py
@@ -0,0 +1,15 @@
+'''from math import sqrt,pi
+r = sqrt(49)*pi
+print(r)'''
+'''
+from math import*
+r = sqrt(49)
+print(r)
+''''''
+import math as m
+r = (m.pi)*22/7
+print(r)
+''''''
+import math
+print(dir(math))'''
+
