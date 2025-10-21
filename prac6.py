269c15b329df832676fefcfb5c6938d5913178ca
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-10-21 18:04:54 +0530

    Practice

---
+++ prac6.py
@@ -0,0 +1,111 @@
+"""print("Hello World")
+a = "Tejas"
+b = "IT"
+c = 5
+d = "10"
+print(a+b)
+"""#print(a+c)
+#print(a+d)"""
+""""print(f"Hello, I am {a} from {b}")
+print("Hello I am \"Tejas\" \nKhaire")
+print("Tejas",6,7,sep="~",end="Khaire")
+print(9//5)#1
+print(9//3)#3
+print(9//7)#1
+print(9//2)#5"""
+
+'''print("Tejas"*5)
+print(Tejas * 5)
+print("Tejas"+5)'''
+
+"""a = input("Enter Your Name: ")
+b = int(input("Enter Your Age: "))
+print(f"My Name is {a} and I am {b} Years Old")
+print(f"Welcome {a}")"""
+"""w = "0123456789"
+q = "Tejas khaire !"
+print(len(w))
+print(w[2:8:2])
+e = q.upper()
+print(e)
+e = q.lower()
+print(e)
+e = q.rstrip("!")
+print(e)
+h = "Tejas","Tejas",0
+print(h.count("Tejas"))
+print(len(h))
+g = "hello world"
+n=g.capitalize()
+print(n)
+n=g.title()
+print(n)"""
+
+#a = int(input("Enter Your Age: "))
+"""if (a > 20):
+    print("Yes")"""
+"""if (a<=18):
+    print("Not Eligible")
+else:
+    print("Yes,Eligible")"""
+"""if a<18:
+    print("Below 18")
+elif 19<a<100:
+    print("Between 19 to 100")
+elif 101<a<200:
+    print("Between 101 to 200")
+else:
+    print("Hello")"""
+
+"""for i in range(20):
+    print(i)
+T = "Tejas"
+for i in T:
+    print(i)
+"""
+"""i = 0
+while i<=5:
+    print(i)
+    i = i + 1
+    """
+"""for i in range(10):
+    print((i+1)*5)
+    if i == 5:
+     continue
+    
+for i in range(10):
+    if i == 5:
+        continue
+    print((i + 1) * 5)"""
+
+"""def hello(a,b):
+    print(a+b)
+hello(5,10)"""
+
+#LIST:
+"""
+list = ["Tejas","Khaire",3081,80]
+print(list)
+print(list[0:3])
+q = [i for i in range(11) if i%2]
+print(q)
+
+q = [1,2,3,4,5,6]
+print(q[1])
+(q.append(7))
+print(q)
+"""
+
+#ef mul(a,b):"""These is a Multipilcation Program"""
+    #print(a*b)
+#mul(2,2)
+#print(mul.__doc__)
+
+"""def fact(n):
+    if (n==0 or n == 1):
+        return 1
+    else:
+        return n*fact(n-1)
+result = fact(5)
+print(result)"""
+
