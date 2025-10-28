89021ab4038b777676efdf85e3638519fe37837e
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-10-28 20:23:03 +0530

    Practice Session

---
+++ Practise.py
@@ -0,0 +1,170 @@
+''''print("Hello \n Tejas Khaire")  #Hello World
+print("Hiii\niiiii")
+'''
+""""print("\"Tejas\"",5,"Khiare")
+"""
+'''print("Tejas",2004,4, 3 , sep=' - ', end = " Khaire")'''
+
+
+'''A = 2**3
+print(A)'''
+'''A = "2"
+B = "2"
+print (int(A)+int (B))
+
+A = 5.5
+B = 5 
+print(A+B)'''
+
+
+'''
+print("Enter Your Num1:")
+A = input(int())
+print("Enter Your Num2:")
+B = input(int())
+print(int(A)+int(B))'''
+
+"""T = "TEjaS"
+for i in T:
+    print(i)
+
+"""
+
+"""Name = "Tejas Khaire"
+print(Name[0:5])
+print(len(Name))
+print(Name.upper())
+print(Name.rstrip("e"))
+print(Name.replace("Tejas","Bandu"))
+print(Name.isalpha())"""
+
+
+'''print("Enter Your DAY: ")
+A = input()
+if (A == "Sunday"):
+    print("You are under Holiday") 
+elif (A == "Tuesday"):
+    print("You are Under 2 DAY")
+elif (A == "Thursday"):
+    print("You are under 4 DAY")
+else:
+   print("You are UnderCover")
+'''
+
+
+'''print("Enter Your Num: ")
+A = int(input())
+match A:
+    case 1:
+        print("NUM IS 1")
+    case 2:
+        print("NUM IS 2")
+    case _:
+        print("NUM IS ",A)'''
+
+'''
+T = "Tejas Khaire"
+for i in T:
+    print(i)  '''
+'''
+for k in range(2,12,1):
+  print(k)
+'''
+"""i = 1
+while(i<=500):
+ print(i)
+ i = i+1
+else:
+ print("Out of Syllabus")"""
+ 
+"""for i in range(10):
+    if(i == 5):
+        continue
+    print(i)"""
+
+#Function in python:
+
+"""def tname (a,b): 
+    print(a+b)
+
+tname(5,4)
+tname(5,25)"""
+
+'''a = int(input("Enter num1: "))
+b = int(input("Enter num2: "))
+sum = a + b
+print("Addition of num1 and num2: ",sum)'''
+"""
+T = range(10,40)
+print(T)
+"""
+'''
+T = "Tejas"
+print(T[1])
+'''
+ls = [0,8,9,3,5,7,3]
+'''print(ls)
+print(ls[0])
+print(ls[1])
+print(ls[2])
+print(ls[3])
+ls2 = [4,5,6,7]
+k = ls + ls2
+print(k)
+ls.sort()
+ls.append(4)
+print(ls.index(9))
+ls.sort(reverse=True)
+ls.reverse
+'''
+'''print(ls.count(3))
+print(ls)
+M = ls.copy()
+print(M)'''\
+
+
+#Tuples:
+
+T = (1,2,2,4,8)
+'''print(len(T))
+print(T[0])
+print(T[1])
+print(T[2])
+print(T[3])
+print(T[4])
+print(T.count(2))
+print(T)'''
+"""if 8 in T:
+    print("Tejas Is Going Good")
+else:
+    print("False")"""
+'''t = T[0:5:2]
+print(t)
+'''
+'''tup = T
+print(tup.append(4))'''
+
+'''t = T.count(2)'''
+'''t = T.index(5)
+print(len(T))
+print(t)'''
+res = T.index(2,1,3)
+print(res)
+
+
+print("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
