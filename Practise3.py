16720f6e0d6c9222e6d12d2d7246b1035f215dea
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-11-04 23:18:04 +0530

    Practice Session 3

---
+++ Practise3.py
@@ -0,0 +1,41 @@
+''''print("Hello    World")
+a = int(input("Enter Your Num: "))
+if (a<18):
+    print('Not allowed for Driving')
+elif(18<a<30):
+    print('You are eligible')
+else:
+    print('GAME OVER')
+
+'''
+#a = 'Tejas khaire'
+'''b = '20'
+print(int(a)+int(b))'''
+'''print(a[6])
+for i in a:
+    print(i)
+print(a[0:8:2])'''
+'''Name = "Tejas"
+surname = "Khaire"
+age = 21
+Home = "Chandrapur"
+
+print(f"Hello Everyone, My Name is {Name} {surname} and I am {age} years old\n I am from {Home} ")
+
+def sum(a,b):
+    print(a+b)
+
+sum (8,7)
+
+add = lambda x,y : x+y
+print(add(8,8))
+
+'''
+print("Hello World")
+i = 1
+while i<101:
+    print(i)
+    i = i + 1
+
+for i in range (100):
+    print(i+1)
