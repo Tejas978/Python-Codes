958eaabb9dabc799f873cdd98afc0705cd955d4b
Tejas978git config --global user.name Tejas978 <khairetejas76@gmail.com>
2025-11-09 23:45:35 +0530

    OPP Advance

---
+++ OppsAdv.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+    def area(self):
+        return 3.14 * self.r ** 2
+
+class Rectangle(Shape):
+    def __init__(self, w, h):
+        self.w, self.h = w, h
+    def area(self):
+        return self.w * self.h
+
+shapes = [Circle(5), Rectangle(4, 6)]
+for s in shapes:
+    print(f"{s.__class__.__name__} Area: {s.area()}")
